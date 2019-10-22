84e5e80179d51b2767db773dc5125bad3b6dca81
RMartires <rohitmartires14@gmail.com>
2019-10-22 23:03:31 +0530

    with small screen

--- calculator.js
+++ calculator.js
@@ -13,7 +13,7 @@ let ans = 0;
 
 
 screen = document.getElementById("screen");
-histscreen = document.getElementById("history");
+smallscreen = document.getElementById("smallsc");
 
 screen.innerHTML = "0";
 for (let i = 0; i < allbuttons.length; i++) {
@@ -35,16 +35,40 @@ for (let key of mainbuttons) {
     keys.addEventListener("click", () => {
         str = `${str}${keys.innerHTML}`;
         screen.innerHTML = str;
+        trytoeval();
     });
 }
 //eventlistener of del but
 document.getElementById(`${sidebuttons[0]}key`).addEventListener("click", () => {
     str = str.slice(0, str.length - 1);
     screen.innerHTML = str;
+    if (str.length > 0) {
+        trytoeval();
+    } else {
+        tempans = 0;
+        tempstr = '';
+        smallscreen.innerHTML = tempstr;
+    }
 });
 //eventlistener of = but
 document.getElementById(`${sidebuttons[1]}key`).addEventListener("click", () => {
     ans = eval(str);
     str = ans.toString();
     screen.innerHTML = str;
+    tempans = 0;
+    tempstr = '';
+    smallscreen.innerHTML = tempstr;
 });
+
+
+function trytoeval() {
+    try {
+        tempans = eval(str);
+        tempstr = tempans.toString();
+        smallscreen.innerHTML = tempstr;
+    } catch (e) {
+        //donothing
+    }
+
+    return null;
+}
