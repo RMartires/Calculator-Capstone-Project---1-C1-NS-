4c80932cb39359c910739de5e1a7a2761eae8c52
RMartires <rohitmartires14@gmail.com>
2019-10-22 22:30:40 +0530

    updated css

--- calculator.js
+++ calculator.js
@@ -1,20 +1,33 @@
 
-let keypad = document.getElementById("keypad");
+let keypadleft = document.getElementById("leftbuts");
+let keypadright = document.getElementById("rightbuts");
+
 let mainbuttons = ["7", "8", "9", "4", "5", "6", "1", "2", "3", ".", "0", "/", "*", "-", "+"];
 let sidebuttons = ["Del", "="];
 let allbuttons = [...mainbuttons, ...sidebuttons];
 let buttons = [];
 let screen;
 let str = '';
+let history = '';
 let ans = 0;
 
+
 screen = document.getElementById("screen");
+histscreen = document.getElementById("history");
+
 screen.innerHTML = "0";
 for (let i = 0; i < allbuttons.length; i++) {
     buttons.push(document.createElement('button'));
     buttons[i].innerText = allbuttons[i];
     buttons[i].id = `${allbuttons[i]}key`;
-    keypad.appendChild(buttons[i]);
+    if ((i >= 0 && i <= 10) || (i == 16)) {
+        buttons[i].className = `leftbuts`;
+        keypadleft.appendChild(buttons[i]);
+    } else {
+        buttons[i].className = `rightbuts`;
+        keypadright.appendChild(buttons[i]);
+    }
+
 }
 //eventlistener of main buts
 for (let key of mainbuttons) {
